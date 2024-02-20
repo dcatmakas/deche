0ca0c8bc9d0956194036d098a0a900f590379e06
Doğukan <dcatmakas@gmail.com>
2024-02-20 18:38:16 +0300

    first commit

---
+++ deche/deche.h
@@ -0,0 +1,18 @@
+//
+//  deche.h
+//  deche
+//
+//  Created by Doğukan Çatmakaş on 20.02.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for deche.
+FOUNDATION_EXPORT double decheVersionNumber;
+
+//! Project version string for deche.
+FOUNDATION_EXPORT const unsigned char decheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <deche/PublicHeader.h>
+
+
